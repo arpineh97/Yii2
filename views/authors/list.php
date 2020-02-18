a47eadb819fa31dcc250e5e646d3537d2cfcff5c
Arpine Harutyunyan <arpine.har@gmail.com>
2020-02-18 17:27:48 +0400

    Yii2

--- views/authors/list.php
+++ views/authors/list.php
@@ -1,12 +1,6 @@
 <?php
 
-use app\models\Authors;
-use app\models\Books;
 use app\widgets\BooksList;
-use kartik\select2\Select2;
-use yii\helpers\ArrayHelper;
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
 
 ?>
 
